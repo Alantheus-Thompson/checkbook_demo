cce90145bfb5db0d18d80ad33e98396ecc502562
Alantheus Thompson <Alantheus.Thompson@gmail.com>
2023-10-31 12:21:21 -0500

    functions

--- checkbook_project_functions.py
+++ checkbook_project_functions.py
@@ -1,97 +1,60 @@
-{
- "cells": [
-  {
-   "cell_type": "code",
-   "execution_count": 1,
-   "id": "fe8fe512",
-   "metadata": {},
-   "outputs": [
-    {
-     "name": "stdout",
-     "output_type": "stream",
-     "text": [
-      "~~~~ Welcome to your terminal checkbook! ~~~~~\n"
-     ]
+import os
+import json 
+
+import os
+import json
+
+def get_or_create_user_profile(first_name, last_name):
+    user_profiles = []
+
+    if os.path.exists('user_profiles.json'):
+        with open('user_profiles.json', 'r') as profiles_file:
+            user_profiles = json.load(profiles_file)
+
+    for user_profile in user_profiles:
+        if user_profile.get("first_name") == first_name and user_profile.get("last_name") == last_name:
+            return user_profile
+
+    new_user_profile = {
+        "first_name": first_name,
+        "last_name": last_name,
     }
-   ],
-   "source": [
-    "def create_user_profile(first_name, last_name, birthday):\n",
-    "    user_profile = {\n",
-    "        \"first_name\": first_name,\n",
-    "        \"last_name\": last_name,\n",
-    "        \"birthday\": birthday,\n",
-    "    }\n",
-    "    with open('user_profile.json', 'w') as profile_file:\n",
-    "        json.dump(user_profile, profile_file)\n",
-    "\n",
-    "def get_user_profile():\n",
-    "    if os.path.exists('user_profile.json'):\n",
-    "        with open('user_profile.json', 'r') as profile_file:\n",
-    "            user_profile = json.load(profile_file)\n",
-    "        return user_profile\n",
-    "    return None\n",
-    "\n",
-    "\n",
-    "def create_ledger_file(user_id):\n",
-    "    ledger_filename = f\"{user_id}_ledger.txt\"\n",
-    "    if not os.path.exists(ledger_filename):\n",
-    "        with open(ledger_filename, 'w') as ledger_file:\n",
-    "            ledger_file.write(\"0.0\")\n",
-    "\n",
-    "\n",
-    "def get_balance():\n",
-    "    with open('ledger.txt', 'r') as ledger_file:\n",
-    "        return float(ledger_file.read())\n",
-    "\n",
-    "def add_debit(amount):\n",
-    "    balance = get_balance()\n",
-    "    balance -= amount\n",
-    "    with open('ledger.txt', 'w') as ledger_file:\n",
-    "        ledger_file.write(str(balance)\n",
-    "\n",
-    "def add_credit(amount):\n",
-    "    balance = get_balance()\n",
-    "    balance += amount\n",
-    "    with open('ledger.txt', 'w') as ledger_file:\n",
-    "        ledger_file.write(str(balance))\n",
-    "\n",
-    "                          \n",
-    "def add_debit(amount):\n",
-    "    amount = -amount  # Make the amount negative for debits\n",
-    "    balance = get_balance()\n",
-    "    balance += amount  # Subtract the debit amount from the balance\n",
-    "    with open('ledger.txt', 'a') as ledger_file:\n",
-    "        ledger_file.write(f\"{amount:.2f}\\n\")\n",
-    "        ledger_file.write(f\"Balance: ${balance:.2f}\\n\")\n",
-    "\n",
-    "def add_credit(amount):\n",
-    "    balance = get_balance()\n",
-    "    balance += amount  # Add the credit amount to the balance\n",
-    "    with open('ledger.txt', 'a') as ledger_file:\n",
-    "        ledger_file.write(f\"{amount:.2f}\\n\")\n",
-    "        ledger_file.write(f\"Balance: ${balance:.2f}\\n\")\n"
-   ]
-  }
- ],
- "metadata": {
-  "kernelspec": {
-   "display_name": "Python 3 (ipykernel)",
-   "language": "python",
-   "name": "python3"
-  },
-  "language_info": {
-   "codemirror_mode": {
-    "name": "ipython",
-    "version": 3
-   },
-   "file_extension": ".py",
-   "mimetype": "text/x-python",
-   "name": "python",
-   "nbconvert_exporter": "python",
-   "pygments_lexer": "ipython3",
-   "version": "3.11.5"
-  }
- },
- "nbformat": 4,
- "nbformat_minor": 5
-}
+
+    user_profiles.append(new_user_profile)
+
+    with open('user_profiles.json', 'w') as profiles_file:
+        json.dump(user_profiles, profiles_file)
+
+    balance_filename = f"{first_name}_{last_name}_balance.txt"
+    with open(balance_filename, 'w') as balance_file:
+        balance_file.write("0.00")
+
+    return new_user_profile
+    
+def get_balance():
+    with open('balance.txt', 'r') as ledger_file:
+        lines = ledger_file.readlines()
+
+    balance = 0.0  
+    for line in reversed(lines):
+        if line.startswith("Balance: $"):
+            balance_str = line.split('$')[1] 
+            balance = float(balance_str) 
+            break  
+
+    return balance
+
+
+def add_debit(amount):
+    balance = get_balance()
+    new_balance = balance - amount
+    with open('balance.txt', 'a') as ledger_file:
+        ledger_file.write(f"-${amount:.2f}\n")
+        ledger_file.write(f"Balance: ${new_balance:.2f}\n")
+
+def add_credit(amount):
+    balance = get_balance()
+    new_balance = balance + amount
+    with open('balance.txt', 'a') as ledger_file:
+        ledger_file.write(f"+${amount:.2f}\n")
+        ledger_file.write(f"Balance: ${new_balance:.2f}\n")
